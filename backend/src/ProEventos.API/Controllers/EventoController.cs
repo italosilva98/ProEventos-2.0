9656d257490ff9c6e326694d8e82e91619a51da5
italosilva98 <italosilvestre3@icloud.com>
2021-08-25 22:05:38 -0300

    Database connection

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,50 +13,21 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController() { }
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-                EventoId = 1,
-                Tema = "Angular",
-                Local = "SP",
-                Lote = "1º Lote",
-                QtdPessoas = 100,
-                DataEvento = DateTime.Now.AddDays(2).ToString()
-            },
-             new Evento() {
-                EventoId = 2,
-                Tema = "C#",
-                Local = "SP",
-                Lote = "1º Lote",
-                QtdPessoas = 100,
-                DataEvento = DateTime.Now.AddDays(3).ToString()
-            }
-        };
-
-        // [HttpGet]
-        // public Evento Get()
-        // {
-        //     return new Evento() {
-        //         EventoId = 1,
-        //         Tema = "Angular",
-        //         Local = "SP",
-        //         Lote = "1º Lote",
-        //         QtdPessoas = 100,
-        //         DataEvento = DateTime.Now.AddDays(2).ToString()
-        //     };
-        // }
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
-        
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
@@ -77,3 +49,49 @@ namespace ProEventos.API.Controllers
         }
     }
 }
+
+
+//moock
+// public IEnumerable<Evento> _evento = new Evento[] {
+//     new Evento() {
+//         EventoId = 1,
+//         Tema = "Angular",
+//         Local = "SP",
+//         Lote = "1º Lote",
+//         QtdPessoas = 100,
+//         DataEvento = DateTime.Now.AddDays(2).ToString()
+//     },
+//      new Evento() {
+//         EventoId = 2,
+//         Tema = "C#",
+//         Local = "SP",
+//         Lote = "1º Lote",
+//         QtdPessoas = 100,
+//         DataEvento = DateTime.Now.AddDays(3).ToString()
+//     }
+// };
+
+// [HttpGet]
+// public Evento Get()
+// {
+//     return new Evento() {
+//         EventoId = 1,
+//         Tema = "Angular",
+//         Local = "SP",
+//         Lote = "1º Lote",
+//         QtdPessoas = 100,
+//         DataEvento = DateTime.Now.AddDays(2).ToString()
+//     };
+// }
+
+// [HttpGet]
+// public IEnumerable<Evento> Get()
+// {
+//     return _evento;
+// }
+
+// [HttpGet("{id}")]
+// public IEnumerable<Evento> GetById(int id)
+// {
+//     return _evento.Where(evento => evento.EventoId == id);
+// }
